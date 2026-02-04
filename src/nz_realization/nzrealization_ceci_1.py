209ecb49ac5cbc3de3327468a52db7b34ebcce4e
Boyan Yin <38168034+BoyanYin@users.noreply.github.com>
2026-02-04 17:47:32 -0500

    Update nzrealization_ceci_1.py

--- src/nz_realization/nzrealization_ceci_1.py
+++ src/nz_realization/nzrealization_ceci_1.py
@@ -3,7 +3,6 @@ from rail.core.data import TableHandle, ModelHandle, QPHandle, Hdf5Handle
 from rail.estimation.estimator import CatEstimator, CatInformer
 #from ceci_example.types import NpyFile, HDFFile
 # We need to define NpyFile etc we want to use inside ceci_example.types, Or not??? get_input don't use this function and we open in run ourselves
-
 class PreparePZRealizationsPipe(PipelineStage):
     """
     Generates LHC sampling points for the uncertainty beside sample variance and shot noise.
@@ -16,10 +15,6 @@ class PreparePZRealizationsPipe(PipelineStage):
     parallel = False
     
     config_options = {
-        "shot_noise": StageParameter(bool, False,
-            msg="If you want to add shot noise from the limited number of galaxies in redshift and deep sample "),
-        "sample_variance": StageParameter(bool, False,
-            msg="If you want to add sample variance from the limited area of redshift and deep field. Must add shot nose if you add sample variance" ),
         "photometric_zeropoint_deep": StageParameter(bool, False,
             msg="If you want to add photometric zero point uncertainty due to the deep field zero point offsets " ),
         "redshift_sample_uncertainty": StageParameter(bool, False,
@@ -28,8 +23,7 @@ class PreparePZRealizationsPipe(PipelineStage):
             msg="If you want to add photometric zero point uncertainty due to the wide field zero point offsets " ),  #not implemented
         "photometric_skybackground_wide": StageParameter(bool, False,
             msg="If you want to add skybackground uncertainty on the wide field photometry " ), #not implemented       
-        "num_lhc_points":StageParameter(int, 100,msg="number of lhc points we want to sample"),
-        "num_3sdir":StageParameter(int, 100,msg="number of 3sdir sampling we want to do"),
+        "num_lhc_points":StageParameter(int, 100,msg="number of lhc points we want to sample")
     }
 
 
@@ -38,18 +32,13 @@ class PreparePZRealizationsPipe(PipelineStage):
             deepfield_zeropoint_filename = self.get_input("deepfield_zeropoint")
             deepfield_zeropoint_data = np.load(deepfield_zeropoint_filename)
             
-            shot_noise = self.config["shot_noise"]
-            sample_variance = self.config["sample_variance"]
             photometric_zeropoint_deep = self.config["photometric_zeropoint_deep"]
             redshift_sample_uncertainty = self.config["redshift_sample_uncertainty"]
             photometric_zeropoint_wide = self.config["photometric_zeropoint_wide"]
             photometric_skybackground_wide = self.config["photometric_skybackground_wide"]
             
             num_lhc_points = self.config["num_lhc_points"]
-            num_3sdir = self.config["num_3sdir"]
             
-            generate_LHC_points(deepfield_zeropoint_data, shot_noise, sample_variance, photometric_zeropoint_deep, redshift_sample_uncertainty, photometric_zeropoint_wide, photometric_skybackground_wide, num_lhc_points )
+            generate_LHC_points(deepfield_zeropoint_data, photometric_zeropoint_deep, redshift_sample_uncertainty, photometric_zeropoint_wide, photometric_skybackground_wide, num_lhc_points, LHC_samples_filename)
             
-            #
-            save_LHC_points here
-            num_3sdir 
+         
