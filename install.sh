868a665b4cdc4a160640412096bffa8076b64c7d
Boyan Yin <38168034+BoyanYin@users.noreply.github.com>
2026-02-12 17:01:22 -0500

    Add pyarrow installation to install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ pip install astropy==7.2
 conda install matplotlib
 conda install numba
 conda install tqdm
+conda install pyarrow
 pip install git+https://github.com/chto/rail_base.git --no-build-isolation 
 pip install git+https://github.com/chto/tables_io.git
 pip install .
