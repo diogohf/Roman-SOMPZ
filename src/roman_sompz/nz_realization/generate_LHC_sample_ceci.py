815d4101b22ae4fcfda1922210102b37b0d646c5
Boyan Yin <by84@duke.edu>
2026-04-07 21:38:57 -0700

    Latin Hypercube Sampling implemented

--- src/roman_sompz/nz_realization/generate_LHC_sample_ceci.py
+++ src/roman_sompz/nz_realization/generate_LHC_sample_ceci.py
@@ -1,29 +1,58 @@
 import healpy as hp
 import numpy as np
+from scipy.stats import qmc
+from scipy.stats import norm
 
-#Currently just use Gaussian random, will change to Latin Hypercube sampling later
 def generate_LHC_points(deepfield_zeropoint_data, widefield_zeropoint_data, sfd_map, lss_error_map, photometric_zeropoint_deep, redshift_sample_uncertainty, photometric_zeropoint_wide, photometric_skybackground_deep, photometric_skybackground_wide, num_lhc_points):
-    print('photometric_zeropoint_wide', photometric_zeropoint_wide)
     LHC_sample = {'deep_zp': None, 'wide_zp': None, 'sky': None}
-    
+
+    len_deepzp = 0
+    len_widezp = 0
+    len_sky = 0
+
+    ## Gathering the info we have
+    LHC_std = []
     if photometric_zeropoint_deep:
-        deep_zp_result = np.random.normal(loc=0, scale=deepfield_zeropoint_data[np.newaxis, :], size=(num_lhc_points, len(deepfield_zeropoint_data)))
-        LHC_sample['deep_zp'] = np.array(deep_zp_result)
-    else:
-        LHC_sample['deep_zp'] = np.zeros((0,len(deepfield_zeropoint_data)))
-        
+        LHC_std += deepfield_zeropoint_data.tolist()
+        len_deepzp = len(deepfield_zeropoint_data)
     if photometric_zeropoint_wide:
-        wide_zp_result = np.random.normal(loc=0, scale=widefield_zeropoint_data[np.newaxis, :], size=(num_lhc_points, len(widefield_zeropoint_data)))
-        LHC_sample['wide_zp'] = np.array(wide_zp_result)
-    else:
-        LHC_sample['wide_zp'] = np.zeros((0,len(widefield_zeropoint_data)))
+        LHC_std += widefield_zeropoint_data.tolist()
+        len_widezp = len(widefield_zeropoint_data)
+    if photometric_skybackground_deep or photometric_skybackground_wide:   
+        LHC_std += [1.0]
+        len_sky = 1
         
-    #Shall use same dust uncertainty map for deep and wide
-    if photometric_skybackground_deep or photometric_skybackground_wide:        
-        gauss_samples = np.random.normal(loc=0, scale=1, size=num_lhc_points)
+    LHC_std = np.array(LHC_std)
+    
+    num_parameters = len(LHC_std)
+
+    if num_parameters!=0:
+        #LHC generation
+        sampler = qmc.LatinHypercube(d=num_parameters)
+        sample = sampler.random(n=num_lhc_points)
+        
+        # Take this uniform distribution as cdf
+        # Transform to pdf
+        sample_gaussian = norm.ppf(sample, loc=0, scale=LHC_std)
+        
+        
+    if photometric_zeropoint_deep:
+        LHC_sample['deep_zp'] = np.array(sample_gaussian[:,0:len_deepzp])
+    else:
+        LHC_sample['deep_zp'] = np.zeros((0,len_deepzp))
+    
+    if photometric_zeropoint_wide:
+        LHC_sample['wide_zp'] = np.array(sample_gaussian[:,len_deepzp : len_deepzp+len_widezp])
+    else:
+        LHC_sample['wide_zp'] = np.zeros((0,len_widezp))
+    
+    if photometric_skybackground_deep or photometric_skybackground_wide:   
+        gauss_samples = np.array(sample_gaussian[:, len_deepzp+len_widezp: len_deepzp+len_widezp+len_sky].flatten())
         # This 10% uncertainty is a coherent uncertainty
         sky_uncertainty = np.array([g * 0.1 * sfd_map for g in gauss_samples])
         # This uncertainty is due to dust map lss correction
+        # Skipping LHS here: at ~100,000 dimensions, LHS loses its space-filling 
+        # advantage and becomes a massive, unnecessary computational bottleneck.
         lss_uncertainty = np.random.normal(loc=0, scale=lss_error_map, size=(num_lhc_points, len(lss_error_map)))
         print(np.shape(sky_uncertainty), np.shape(lss_uncertainty))
         sky_result = sky_uncertainty + lss_uncertainty
